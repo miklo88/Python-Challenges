150ea690645d7b9a86ee4ee10711f65fbb431b73
Carl Redding <credding07@gmail.com>
2020-06-21 18:37:10 -0400

    looking more into code abbey's problem. moving onto pynatives 10 for and while loop challenges

---
+++ Pynative.py
@@ -0,0 +1,90 @@
+# Exercise Question 1: Print First 10 natural numbers using while loop
+#1. expected output 
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+#answer
+x = 1
+while x < 11:
+    print(x)
+    x += 1
+    #Exercise Question 2: Print the following pattern
+#2.expected output 
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5
+
+#Exercise Question 3: Accept number from user and calculate the sum of all number between 1 and given number
+#3.expected output For example user given 10 so the output should be 55
+
+# Exercise Question 4: Print multiplication table of given number
+#4.expected output For example num = 2 so the output should be
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+# 20
+
+# Exercise Question 5: Given a list iterate it and display numbers which are divisible by 5 and if you find number greater than 150 stop the loop iteration
+#5.expected output list1 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+# 15
+# 55
+# 75
+# 150
+
+# Exercise Question 6: Given a number count the total number of digits in a number
+#6.expected output For example, the number is 75869, so the output should be 5.
+
+# Exercise Question 7: Print the following pattern using for loop
+#7.expected output 
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1
+
+# Exercise Question 8: Reverse the following list using for loop
+#8.expected output list1 = [10, 20, 30, 40, 50]
+# 50
+# 40
+# 30
+# 20
+# 10
+
+# Exercise Question 9: Display -10 to -1 using for loop
+#9.expected output 
+# -10
+# -9
+# -8
+# -7
+# -6
+# -5
+# -4
+# -3
+# -2
+# -1
+
+# Exercise Question 10: Display a message “Done” after successful execution of for loop
+#10.expected output 
+# for i in range(5):
+#     print(i)
+# 0
+# 1
+# 2
+# 3
+# 4
+# Done!
---
+++ Whiteboard.py
@@ -0,0 +1,66 @@
+#CODE ABBEY 1
+#print the sum of all the numbers in the list.
+numbers = [658, 1263, 1077, 806, 1083, 842, 751, 425, 238, 97, 449, 641, 908, 399, 1296, 796, 1256, 638, 682, 324, 295, 1048, 123, 439, 929, 499, 425, 974, 853, 676, 485, 210, 638, 261, 1007, 421, 1094, 458, 836, 32, 545, 1276, 664]
+#easy way of doing it
+print(sum(numbers)) #28817
+#doing it in a function
+def sum_of_nums(nums):
+    total = 0
+    for i in nums:
+        total = total + i
+    return total
+numbers = [658, 1263, 1077, 806, 1083, 842, 751, 425, 238, 97, 449, 641, 908, 399, 1296, 796, 1256, 638, 682, 324, 295, 1048, 123, 439, 929, 499, 425, 974, 853, 676, 485, 210, 638, 261, 1007, 421, 1094, 458, 836, 32, 545, 1276, 664]
+print(sum_of_nums(numbers)) #28817
+
+
+###CODE ABBEY 2
+### print the sum of each list added to one another
+numbers1 = [505058, 851325, 706933, 873782, 134329, 685105, 309121, 551519, 66974, 531212, 651845, 13308]
+numbers2 = [940896, 411618, 151664, 487095, 140746, 391224, 591185, 865793, 452347, 816587, 321173, 406687]
+results = []
+
+for (number1, number2) in zip(numbers1,numbers2):
+    results.append(number1+number2)
+
+print(results)
+#return [1445954, 1262943, 858597, 1360877, 275075, 1076329, 900306, 1417312, 519321, 1347799, 973018, 419995]
+    #this gives me 12 lists of the same list numbers1
+    #for loops loop how many times given to them. if no range, they loop by default num of elements in the list.
+
+# TRY AGAIN WITHOUT RETURNING LIST
+a = 294481, 594273, 868711, 841704, 308452, 265913, 675622, 392854, 13626, 545908, 338023, 944651, 123590, 148218, 239998 
+b = 522092, 838653, 273260, 87247, 871267, 858192, 248003, 219925, 992411, 848315, 178179, 740628, 915433, 800864, 476303
+#the sum of each num in tuple a + tuple b
+# print(tuple(sum(num) for num in zip(a,b)))
+for num in zip(a,b):
+    print(sum(num)
+ #returns
+#  816573
+# 1432926
+# 1141971
+# 928951
+# 1179719
+# 1124105
+# 923625
+# 612779
+# 1006037
+# 1394223
+# 516202
+# 1685279
+# 1039023
+# 949082
+# 716301
+
+# for num1A in a:
+#     print(f'List A: {num1A}')
+# for num1B in b:
+#     print(f'List B: {num1B}')
+
+
+# print(len(a))
+# print(type(b))
+# print(type(c))
+# print(sum(a))
+# c = a[0]+b[0]
+# print(c)
+# print(b[0])
